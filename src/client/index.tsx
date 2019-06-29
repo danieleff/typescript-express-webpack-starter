50d6f8414aaac27107b818136f316ff6ddfd6ea0
Daniel Fekete <danieleff@gmail.com>
2019-06-29 13:49:20 +0200

    Use webpack proxy to proxy API calls to backend

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,6 +3,4 @@ console.log("Loaded");
 const root = document.getElementById("root")!;
 root.innerHTML = "Loaded";
 
-declare var WEBPACK_DEFINE_SERVER_URL: string;
-
-fetch(WEBPACK_DEFINE_SERVER_URL + "/api/test").then(res => res.text()).then(text => root.innerHTML = text);
+fetch("/api/test").then(res => res.text()).then(text => root.innerHTML = text);
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,21 @@
-var serverUrl = "";
-
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = (env, argv) => {
     
-    const isDev = argv.mode === "development";
-
-    if (isDev) {
-        serverUrl = "http://localhost:3000";
-    }
-
     return {
         entry: "./src/client/index.tsx",
         plugins: [
             new HtmlWebpackPlugin({
                 template: "./src/client/index.html"
             }),
-            new webpack.DefinePlugin({
-                WEBPACK_DEFINE_SERVER_URL: JSON.stringify(serverUrl)
-            }),
             new CleanWebpackPlugin(['build/client']),
         ],
         output: {
             filename: "hashed/[name].[hash:8].js",
-            path: path.resolve(__dirname, 'build/client')
+            path: path.resolve(__dirname, 'build/client'),
+            publicPath: "/",
         },
         resolve: {
             extensions: [".ts", ".tsx", ".js"]
@@ -39,6 +27,10 @@ module.exports = (env, argv) => {
         },
         devServer: {
             disableHostCheck: true,
+            historyApiFallback: true,
+            proxy: {
+                "/api": "http://localhost:3000"
+            }
         }
     }
 };
